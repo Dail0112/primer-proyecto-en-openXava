734d9e349c2331809088a662e7e940a128f93fdd
sofia <sofiavargasayerdis>
2025-11-03 23:08:15 -0600

    Leccion 2.2

---
+++ src/main/java/ni/edu/uam/facturacion/modelo/Factura.java
@@ -0,0 +1,58 @@
+package ni.edu.uam.facturacion.modelo;
+
+import calculadores.CalculadorSiguienteNumeroParaAnyo;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import org.openxava.calculators.CurrentLocalDateCalculator;
+import org.openxava.calculators.CurrentYearCalculator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Collection;
+
+public class Factura {
+
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @Hidden
+    @GenericGenerator(name="system-uuid", strategy="uuid")
+    @Column(length=32)
+    String oid;
+
+    @Column(length=4)
+    @DefaultValueCalculator(CurrentYearCalculator.class) // Año actual
+    int anyo;
+
+    @Column(length=6)
+    int numero;
+
+    @Required
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class) // Fecha actual
+    LocalDate fecha;
+
+    @TextArea
+    String observaciones;
+
+    @Column(length=6)
+    @DefaultValueCalculator(value= CalculadorSiguienteNumeroParaAnyo.class,
+            properties=@PropertyValue(name="anyo") // Para inyectar el valor de anyo de Factura
+            // en el calculador antes de llamar a calculate()
+    )
+    int numero;
+
+    @Required
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class) // Fecha actual
+    LocalDate fecha;
+
+    @TextArea
+    String observaciones;
+
+    @ElementCollection
+    @ListProperties("producto.numero, producto.descripcion, cantidad")
+    Collection<Detalle> detalles;
+
+    @ManyToOne(fetch=FetchType.LAZY, optional=false)
+    @ReferenceView("Simple") // La vista llamada 'Simple' se usará para visualizar esta referencia
+    Cliente cliente;
+
+}
